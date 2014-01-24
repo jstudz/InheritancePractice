77c3eb0bd0ee6aada81770f142c1c579081dc1c9
Arthas <Arthas@Arthas-PC>
2014-01-24 16:49:31 -0600

    Added Variables and Methods to Employee on Concrete package

--- src/my/concrete/Employee.java
+++ src/my/concrete/Employee.java
@@ -3,5 +3,33 @@
 package my.concrete;
 
 public class Employee {
+    private String name;
+    private int employeeID;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
     
 }
--- src/myabstract/SalaryPlusCommissionEmployee.java
+++ src/myabstract/SalaryPlusCommissionEmployee.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package myabstract;
 
-/**
- *
- * @author Arthas
- */
 public class SalaryPlusCommissionEmployee extends Employee{
     
 }
