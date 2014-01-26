0e6b11087a60251d4234e2c16f4cd73c03c36af5
Arthas <Arthas@Arthas-PC>
2014-01-26 09:20:21 -0600

    Finished the methods for the interface Employee class. Implementing them into the HourlyEmployee class

--- src/myinterface/Employee.java
+++ src/myinterface/Employee.java
@@ -6,6 +6,8 @@ public interface Employee {
        String name = "";
        int employeeID = 0;
     
+    public void setName(String name);
+    public void setEmployeeID(int employeeID);
     public String getName();
     public int getEmployeeID();
     
--- src/myinterface/HourlyEmployee.java
+++ src/myinterface/HourlyEmployee.java
@@ -4,6 +4,24 @@ package myinterface;
 
 
 public class HourlyEmployee implements Employee {
+    private int hourlyWage;
+    private int hoursWorked;
+
+    public int getHourlyWage() {
+        return hourlyWage;
+    }
+
+    public void setHourlyWage(int hourlyWage) {
+        this.hourlyWage = hourlyWage;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
     
     public String getName() {
             return name;
@@ -11,6 +29,14 @@ public class HourlyEmployee implements Employee {
     
     public int getEmployeeID() {
         return employeeID;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setEmployeeID(int employeeNum) {
+       this.employeeID = employeeID;
     }
 }
     
