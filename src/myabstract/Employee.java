c310ee4c5c350867144d73a668d776223c1c393c
Arthas <Arthas@Arthas-PC>
2014-01-26 18:42:41 -0600

    Wrote the abstract classes other than Employee

--- src/myabstract/Employee.java
+++ src/myabstract/Employee.java
@@ -3,5 +3,8 @@
 package myabstract;
 
 public abstract class Employee {
+    private String name;
+    private int employeeID;
+    
     
 }
--- src/myabstract/HourlyEmployee.java
+++ src/myabstract/HourlyEmployee.java
@@ -3,5 +3,24 @@
 package myabstract;
 
 public class HourlyEmployee extends Employee{
+    private int payRate;
+    private int hoursWorked;
+
+    public int getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(int payRate) {
+        this.payRate = payRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+    
     
 }
