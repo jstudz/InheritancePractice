f39fb49af3301ae747771f7b54d6146fc4088f7f
Arthas <Arthas@Arthas-PC>
2014-01-29 21:06:25 -0600

    added one more property to abstract package

--- src/myinterface/Employee.java
+++ src/myinterface/Employee.java
@@ -5,9 +5,13 @@ package myinterface;
 public interface Employee  {
      
     public abstract void setName(String name);
+    
     public abstract void setEmployeeID(int employeeID);
+    
     public abstract String getName();
+    
     public abstract int getEmployeeID();
+    
     public abstract int getPay();
     
 }
